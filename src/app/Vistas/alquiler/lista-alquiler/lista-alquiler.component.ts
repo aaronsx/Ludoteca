b2f7f403b2b86022d397185c51cd93837b4c7990
Aaron <senen.00@gmail.com>
2023-11-20 14:54:14 +0100

    20/11/23

--- src/app/Vistas/alquiler/lista-alquiler/lista-alquiler.component.ts
+++ src/app/Vistas/alquiler/lista-alquiler/lista-alquiler.component.ts
@@ -1,24 +1,57 @@
 import { Component } from '@angular/core';
+import { forkJoin } from 'rxjs';
 import { Alquiler } from 'src/app/Modelos/mock-alquiler';
+import { Juego } from 'src/app/Modelos/mock-juego';
+import { Usuario } from 'src/app/Modelos/mock-usuario';
 import { FireBaseService } from 'src/app/Servicios/fire-base.service';
 
 @Component({
   selector: 'app-lista-alquiler',
   templateUrl: './lista-alquiler.component.html',
-  styleUrls: ['./lista-alquiler.component.css']
+  styleUrls: ['./lista-alquiler.component.css'],
 })
 export class ListaAlquilerComponent {
-  alquileres:Alquiler[]=[];
-  constructor(private fbs:FireBaseService) {
-    
+  alquileres: any[] = [];
+  juegos: Juego = { titulo: '', genero: '', pegi: 0, company: '', foto: '' };
+  usuarios: Usuario = { nombre: '', apellido: '', foto: '', email: '' };
+  mostrarTodo: any[] = [];
+
+  constructor(private fbs: FireBaseService) {}
+  ngOnInit() {
+    this.fbs.getFireBase('Alquiler').subscribe((res) => {
+      this.alquileres = res;
+
+      // Para cada alquiler, busca el usuario y el juego correspondientes
+      this.alquileres.forEach((alquiler) => {
+        this.fbs
+          .getFireBasePorId('Usuarios', alquiler.id_Usuario)
+          .subscribe((usuario) => {
+            alquiler.nombreUsuario = usuario.nombre; // Suponiendo que el objeto Usuario tiene una propiedad 'nombre'
+            // Llena el arreglo mostrarTodo con la información actualizada
+            this.mostrarTodo = this.alquileres.map((alquiler) => ({
+              nombreUsuario: alquiler.nombreUsuario || 'Usuario no encontrado',
+            }));
+          });
+
+        this.fbs
+          .getFireBasePorId('Juegos', alquiler.id_Juego)
+          .subscribe((juego) => {
+            alquiler.nombreJuego = juego.titulo; // Suponiendo que el objeto Juego tiene una propiedad 'titulo'
+            // Llena el arreglo mostrarTodo con la información actualizada
+            this.mostrarTodo = this.alquileres.map((alquiler) => ({
+              
+              nombreJuego: alquiler.nombreJuego || 'Juego no encontrado',
+            }));
+          });
+      });
+    });
+    // Llena el arreglo mostrarTodo con la información actualizada
+    this.mostrarTodo = this.alquileres.map(alquiler => ({
+      id: alquiler.id,
+    }));
   }
-  ngOnInit(){
-    this.fbs.getFireBase("Alquiler")
-            .subscribe(res => this.alquileres = res);
+
+  eliminaJuegos(alquileres: Alquiler) {
+    this.fbs.deleteFireBase(alquileres, 'Alquiler');
   }
-    
-    eliminaJuegos(alquileres:Alquiler)
-    {
-      this.fbs.deleteFireBase(alquileres, "Alquiler");
-    }
 }
